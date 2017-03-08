deeec4d99a9853aa2f9612c85057ae8970f53efb
Darran Kelinske <darran.kelinske@gmail.com>
2017-03-07 20:53:01 -0600

    fix mock datamodule

--- app/src/mock/java/com/austindroids/austinfeedsme/modules/DataModule.java
+++ app/src/mock/java/com/austindroids/austinfeedsme/modules/DataModule.java
@@ -1,5 +1,7 @@
 package com.austindroids.austinfeedsme.modules;
 
+import android.content.Context;
+
 import com.austindroids.austinfeedsme.data.EventsDataSource;
 import com.austindroids.austinfeedsme.data.EventsRepository;
 import com.austindroids.austinfeedsme.data.eventbrite.FakeEventbriteDataSource;
@@ -19,6 +21,12 @@ public class DataModule {
     private static final String FIREBASE_URL =
             "https://austin-feeds-me-ec3e5.firebaseio.com//events";
 
+    private final Context context;
+
+    public DataModule(Context context) {
+        this.context = context;
+    }
+
     @Provides @Singleton
     DatabaseReference firebase() {
         return FirebaseDatabase.getInstance().getReferenceFromUrl(FIREBASE_URL);
@@ -31,7 +39,7 @@ public class DataModule {
 
     @Provides @Named("meetup") @Singleton
     EventsDataSource meetupDataSource() {
-        return new MeetupDataSource();
+        return new MeetupDataSource(context);
     }
 
     @Provides @Named("eventbrite") @Singleton
