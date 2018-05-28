710fc323e5d15da095cac046bada232256316bdd
Darran Kelinske <darran.kelinske@gmail.com>
2018-05-27 23:08:48 -0500

    introduce timber

---
+++ app/src/main/java/com/austindroids/austinfeedsme/common/logging/CrashlyticsTree.kt
@@ -0,0 +1,31 @@
+package com.austindroids.austinfeedsme.common.logging
+
+import android.support.annotation.Nullable
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+import timber.log.Timber
+
+class CrashlyticsTree : Timber.Tree() {
+
+    override fun log(priority: Int, @Nullable tag: String?, @Nullable message: String, @Nullable t: Throwable?) {
+        if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+            return
+        }
+
+        Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority)
+        Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag)
+        Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message)
+
+        if (t == null) {
+            Crashlytics.logException(Exception(message))
+        } else {
+            Crashlytics.logException(t)
+        }
+    }
+
+    companion object {
+        private const val CRASHLYTICS_KEY_PRIORITY = "priority"
+        private const val CRASHLYTICS_KEY_TAG = "tag"
+        private const val CRASHLYTICS_KEY_MESSAGE = "message"
+    }
+}
